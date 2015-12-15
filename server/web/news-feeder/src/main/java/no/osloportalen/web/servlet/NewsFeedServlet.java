f9b2a0c486d581457ae89d0a5c0c67d65757a80c
Kjellis <kjell.arne@arvesen.org>
2015-12-15 12:36:33 +0100

    Demo of basic servlet with jetty plugin

--- server/web/news-feeder/src/main/java/no/osloportalen/web/servlet/NewsFeedServlet.java
+++ server/web/news-feeder/src/main/java/no/osloportalen/web/servlet/NewsFeedServlet.java
@@ -1,11 +1,9 @@
 package no.osloportalen.web.servlet;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/base" })
 public class NewsFeedServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6408424569798224350L;
