bcb048e6a03afd8fc80761d28fc50a93594fc90f
Kjellis <kjell.arne@arvesen.org>
2016-01-02 19:09:19 +0100

    Just changed storage engine and continued to work to deliver news content as JSON

--- server/web/news-feeder/src/main/java/no/osloportalen/web/servlet/NewsFeedServlet.java
+++ server/web/news-feeder/src/main/java/no/osloportalen/web/servlet/NewsFeedServlet.java
@@ -2,23 +2,27 @@ package no.osloportalen.web.servlet;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.lightcouch.CouchDbClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.lightcouch.NoDocumentException;
 
 import com.google.gson.Gson;
 
-import no.osloportalen.storage.couchdb.CouchDBFactory;
+import no.osloportalen.storage.BasicStorageFactory;
+import no.osloportalen.storage.couchdb.repository.NewsContentRepository;
 import no.osloportalen.storage.model.NewsContent;
 
 public class NewsFeedServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6408424569798224350L;
+	private static final Logger logger = LogManager.getLogger( NewsFeedServlet.class.getName() );
 
 	public NewsFeedServlet() {
 		super();
@@ -30,27 +34,20 @@ public class NewsFeedServlet extends HttpServlet {
 		response.setContentType("application/JSON");
 		writer.append("Hello world!");
 
-		CouchDbClient client = CouchDBFactory.get();
-		List<NewsContent> contentList = null;
-		System.out.println(client.getDBUri().toString());
-		// ... and this is why we have services..
+		NewsContentRepository newsRepo = BasicStorageFactory.getNewsContentRepository();
+		List<NewsContent> contentList = new ArrayList<NewsContent>();
 		try {
-			contentList = client.view("articles/all").query(NewsContent.class);
-			Integer intCount = client.view("articles/all").queryForInt();
-			System.out.println(intCount);
+			contentList = newsRepo.getAll();
+			logger.debug("We found " + contentList.size() + " news articles");
 		} catch (NoDocumentException nde) {
 			System.out.println("Aww snap! No such document existed.. what the begeeeezes: " + nde.toString());
 			return;
 		}
 		
-		System.out.println("COntentlist = " + contentList.size());
 		Gson gson = new Gson();
 		for (NewsContent content : contentList) {
-			System.out.println(content);
 			writer.write(gson.toJson(content));
 		}
-		// client.find(NewsContent.class, id, params);
 		response.getWriter().write(writer.toString());
 	}
-
 }
