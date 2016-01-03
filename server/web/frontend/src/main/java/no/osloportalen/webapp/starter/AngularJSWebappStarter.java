fd40f6602c8c27b20330a9e55b0485ab6a54f29d
Kjellis <kjell.arne@arvesen.org>
2016-01-03 15:56:02 +0100

    Added a wrapper for the main AngularJS application

---
+++ server/web/frontend/src/main/java/no/osloportalen/webapp/starter/AngularJSWebappStarter.java
@@ -0,0 +1,31 @@
+package no.osloportalen.webapp.starter;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class AngularJSWebappStarter {
+	public static void main(String[] args) throws Exception {
+		// The simple Jetty config here will serve static content from the
+		// webapp directory
+		String webappDirLocation = "src/main/webapp/";
+
+		// The port that we should run on can be set into an environment
+		// variable
+		// Look for that variable and default to 8080 if it isn't there.
+		String webPort = System.getenv( "PORT" );
+		if ( webPort == null || webPort.isEmpty() ) {
+			webPort = "9000";
+		}
+		Server server = new Server( Integer.valueOf( webPort ) );
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath( "/" );
+		webapp.setDescriptor( webappDirLocation + "/WEB-INF/web.xml" );
+		webapp.setResourceBase( webappDirLocation );
+
+		server.setHandler( webapp );
+		server.start();
+		server.join();
+	}
+
+}
