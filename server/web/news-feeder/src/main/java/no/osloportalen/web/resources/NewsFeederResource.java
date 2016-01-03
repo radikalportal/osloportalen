b6875dca49bf1eed1e3bce08bc82238dc216a0a9
Kjellis <kjell.arne@arvesen.org>
2016-01-03 13:12:23 +0100

    Removed test model

--- server/web/news-feeder/src/main/java/no/osloportalen/web/resources/NewsFeederResource.java
+++ server/web/news-feeder/src/main/java/no/osloportalen/web/resources/NewsFeederResource.java
@@ -1,22 +1,18 @@
 package no.osloportalen.web.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Optional;
 
 import no.osloportalen.storage.BasicStorageFactory;
 import no.osloportalen.storage.couchdb.repository.NewsContentRepository;
 import no.osloportalen.storage.model.NewsContent;
-import no.osloportalen.web.models.NewsContentTest;
 
 @Path("/news-feeder")
 @Produces(MediaType.APPLICATION_JSON)
@@ -29,15 +25,6 @@ public class NewsFeederResource {
 		this.template = template;
 		this.defaultName = defaultName;
 		this.counter = new AtomicLong();
-	}
-
-	@GET
-	@Timed
-	public NewsContentTest sayHello(@QueryParam("name") Optional<String> name) {
-		final String value = String.format( template, name.or( defaultName ) );
-		NewsContentTest contentTest = new NewsContentTest();
-		contentTest.setName( value );
-		return contentTest;
 	}
 
 	@Path("/all")
