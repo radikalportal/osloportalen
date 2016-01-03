3dc8a174f14645ede839f8fadb51600a10b248e2
Kjellis <kjell.arne@arvesen.org>
2016-01-03 17:14:26 +0100

    Fixed serving of static css files

--- server/web/frontend/src/main/java/no/osloportalen/webapp/starter/AngularJSWebappStarter.java
+++ server/web/frontend/src/main/java/no/osloportalen/webapp/starter/AngularJSWebappStarter.java
@@ -1,7 +1,16 @@
 package no.osloportalen.webapp.starter;
 
+import java.io.File;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
 
 public class AngularJSWebappStarter {
 	public static void main(String[] args) throws Exception {
@@ -18,14 +27,46 @@ public class AngularJSWebappStarter {
 		}
 		Server server = new Server( Integer.valueOf( webPort ) );
 
-		WebAppContext webapp = new WebAppContext();
-		webapp.setContextPath( "/" );
-		webapp.setDescriptor( webappDirLocation + "/WEB-INF/web.xml" );
-		webapp.setResourceBase( webappDirLocation );
+		// WebAppContext webapp = new WebAppContext();
+		// webapp.setContextPath( "/" );
+		// webapp.setDescriptor( webappDirLocation + "/WEB-INF/web.xml" );
+		// webapp.setResourceBase( webappDirLocation );
 
-		server.setHandler( webapp );
+		ContextHandler baseContext = new ContextHandler();
+		baseContext.setContextPath( "/" );
+		baseContext.setResourceBase( webappDirLocation );
+
+		ContextHandler cssContext = new ContextHandler();
+		cssContext.setContextPath( "/css" );
+		cssContext.setResourceBase( webappDirLocation );
+		
+		ResourceHandler baseResourceHandler = new ResourceHandler();
+
+		baseResourceHandler.setDirectoriesListed( true );
+		baseResourceHandler.setWelcomeFiles( new String[] { "index.html" } );
+		baseResourceHandler.setResourceBase( webappDirLocation );
+		baseContext.setHandler( baseResourceHandler );
+
+		ResourceHandler cssResourceHandler = new ResourceHandler();
+		cssResourceHandler.setDirectoriesListed( true );
+		cssResourceHandler.setResourceBase( webappDirLocation + "/css" );
+		
+		cssContext.setHandler( cssResourceHandler );
+
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers( new Handler[] { baseContext, cssContext } );
+		// handlers.setHandlers( new Handler[]{ cssContextHandler, new
+		// DefaultHandler() } );
+		// handlers.setHandlers(new Handler[] { resource_handler, new
+		// DefaultHandler() });
+
+		server.setHandler( handlers );
+
+		// server.setHandler( webapp );
 		server.start();
+		System.out.println(server.dump());
 		server.join();
+
 	}
 
 }
