fe3c68d3ea4decbfc75dd70427ff0ae10c0fdf70
Kjellis <kjell.arne@arvesen.org>
2016-01-17 16:54:54 +0100

    Updated configuration and main class

--- server/web/frontend/src/main/java/no/osloportalen/webapp/starter/AngularJSWebappStarter.java
+++ server/web/frontend/src/main/java/no/osloportalen/webapp/starter/AngularJSWebappStarter.java
@@ -4,29 +4,33 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import no.osloportalen.webapp.config.DefaultWebConfig;
 import spark.servlet.SparkApplication;
-
+import static spark.Spark.*;
 
 public class AngularJSWebappStarter implements SparkApplication {
-	public static void main(String[] args) throws Exception {
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext( AngularJSWebappStarter.class );
-//		ImmutarePollService service = ctx.getBean( ImmutarePollService.class );
-		// List<ScoreCard> scoreCards = service.calculateInteresetResponse();
-
-		new DefaultWebConfig(  );
-		ctx.registerShutdownHook();
-		ctx.close();
-	}
-
+	
+	public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
 	@Override
 	public void init() {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext( AngularJSWebappStarter.class );
-//		ImmutarePollService service = ctx.getBean( ImmutarePollService.class );
+		// ImmutarePollService service = ctx.getBean( ImmutarePollService.class
+		// );
 		// List<ScoreCard> scoreCards = service.calculateInteresetResponse();
 
-		new DefaultWebConfig(  );
+		new DefaultWebConfig();
 		ctx.registerShutdownHook();
 		ctx.close();
 	}
 
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if ( processBuilder.environment().get( "PORT" ) != null ) {
+			return Integer.parseInt( processBuilder.environment().get( "PORT" ) );
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on
+						// localhost)
+	}
 
 }
